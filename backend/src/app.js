e625799106a554b3cd4cb9f4f08e27057fa9ceda
Delta Student <deepaks9631342845@gmail.com>
2025-03-04 12:42:32 +0530

    writing auth middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,11 @@
 const express = require("express");
 const app= express();
 
-app.use("/test", (req, res)=>{
+// this will match all the http method Api call to user
+app.use("/deepak", (req, res)=>{
   res.send("Hello from seerver");
 })
-app.use("/deepak", (req, res)=>{
-  res.send("hello from deepak")
-})
-app.use("/deepika",(req, res)=>{
-  res.send("hello from deepika")
-})
+app.get
 app.listen((3000), ()=>{
   console.log("server is successfully listening on poprt 300...");
 });
