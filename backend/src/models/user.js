3fca7c7905f257d67008524c67779e08e2c30fc8
Delta Student <deepaks9631342845@gmail.com>
2025-03-11 22:21:53 +0530

    connectionModel

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -38,6 +38,10 @@ const userSchema= new mongoose.Schema({
   },
   gender:{
     type:String,
+    enum:{
+      values:["male", "female", "others"],
+      message:'{VALUE} is not correct',
+    },
     validate(value){
       if(!["male","female","others"].includes(value)){
         throw new Error("Gender data is not valid");
--- backend/src/routes/request.js
+++ backend/src/routes/request.js
@@ -2,10 +2,39 @@ const express= require("express");
   const requestRouter=express.Router();
   const {userAuth}=require("../middleware/auth");
   const User=require("../models/user");
+  const ConnectionRequest=require("../models/connectionRequest")
 
 
-  app.post("/sendconnectionrequest", userAuth, async(req, res)=>{
-    const senderId=req.user._id;
+  app.post("/request/send/:status/:toUserId", userAuth, async(req, res)=>{
+    try {
+      const fromUserId=req.user.Id;
+      const toUserId=req.params.toUserId;
+      const status=req.params.status;
+
+
+      const allowedStatus=["ignored", "interested"];
+      if(!allowedStatus.includes(status)){
+        return res.status(400).json({message :  "Invalid status type : " + status});
+      }
+       // if there ia an existing connectionrequest
+      const existingConnectionrequest= await ConnectionRequest.findOne({
+        fromUserId,
+        toUserId,
+        status
+      })
+
+      const connectionRequest=new connectionRequest({
+        fromUserId,
+        toUserId,
+        status,
+      });
+
+      const data=await connectionRequest.save();
+      res.json({message:"connection request sent successfully", data,});      
+    } catch (error) {
+      res.status(400).send("Error: "+err.message);
+    }
+    res.send(User.firstName + "sent the connection request");
   
   });
 
